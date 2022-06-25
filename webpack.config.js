c317eaa631b61e7eb9b0d969c06b82a3dcdb8709
LidaChk <lvglazko@gmail.com>
2022-06-25 12:00:59 +0300

    feat: 🦄 base page with map

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,77 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const miniCss = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV; //== "production";
+const isProduction = process.env.NODE_ENV; //= = "production";
 
 const config = {
-  entry: ["./src/index.js", "./src/sass/main.scss"],
+  entry: ['./src/index.js', './src/sass/main.scss'],
   output: {
-    path: !isProduction? path.resolve(__dirname, "dist") : path.resolve(__dirname, "pages/page"),
-    filename: 'bundle.js'
+    path: !isProduction
+      ? path.resolve(__dirname, 'dist')
+      : path.resolve(__dirname, 'page'),
+    filename: 'bundle.js',
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new miniCss({
       filename: 'main.css',
-   })
-
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: path.resolve(__dirname, 'page/assets'),
+        },
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
-/*       {
+      /*       {
         test: /\.(js|jsx)$/i,
         loader: "babel-loader",
       }, */
       {
         test: /\.s[ac]ss$/i,
-        use: [miniCss.loader, "css-loader", "sass-loader"],
+        use: [miniCss.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/svg/[name][ext]'
-        }
+          filename: 'assets/svg/[name][ext]',
+        },
       },
       {
         test: /\.(woff|woff2|ttf|otf|eot)$/,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/fonts/[name][ext]'
-        }
+          filename: 'assets/fonts/[name][ext]',
+        },
       },
-
     ],
   },
 };
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production';
   } else {
-    config.mode = "development";
+    config.mode = 'development';
   }
   return config;
 };
